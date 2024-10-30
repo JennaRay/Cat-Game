f7022263e8452cb5e96f25ba18b9a44c10a57c03
Jenna Ray <jennastingray@gmail.com>
2024-10-29 22:48:02 -0600

    added menu

--- object.py
+++ object.py
@@ -1,4 +1,5 @@
 import pygame
+import spritesheet
 
 class Object(pygame.sprite.Sprite):
 
@@ -13,3 +14,16 @@ class Object(pygame.sprite.Sprite):
 
     def draw(self, surface):
         surface.blit(self.image, self.position)
+
+class Button(Object):
+
+    def __init__(self, x, y, image_path, scale=(48, 96)):
+
+        self.sprite_sheet_image = pygame.image.load(image_path).convert_alpha()
+        self.unpressed = spritesheet.SpriteSheet(self.sprite_sheet_image).get_image(0, 192, 96)
+        self.pressed = spritesheet.SpriteSheet(self.sprite_sheet_image).get_image(1, 192, 96)
+        self.image = self.unpressed
+        self.position = [x, y]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
